628cf91a83c774a97ab8e38ce7d329d24ea2f92b
Filippo <corti.filippo03@gmail.com>
2023-09-27 15:32:56 +0200

    first commit

---
+++ First/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int x = 0; x < n; x++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
---
+++ Laboratori/Lab1/Segno/Segno.java
@@ -0,0 +1,10 @@
+package Laboratori.Lab1.Segno;
+public class Segno {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        if (n > 0)
+            System.out.print("+");
+        System.out.println(n);
+    }
+}
