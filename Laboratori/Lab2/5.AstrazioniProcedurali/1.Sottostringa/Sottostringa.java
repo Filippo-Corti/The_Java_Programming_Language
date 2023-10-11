20a80b821d00d8b67822727247dbfba93d769ede
Filippo <corti.filippo03@gmail.com>
2023-10-11 19:58:17 +0200

    Lab2 - Fattoriale

--- Laboratori/Lab2/5.AstrazioniProcedurali/1.Sottostringa/Sottostringa.java
+++ Laboratori/Lab2/5.AstrazioniProcedurali/1.Sottostringa/Sottostringa.java
@@ -1,4 +1,5 @@
 public class Sottostringa {
+    //OVERVIEW: static class that contains methods to find whether a string is substring of another string
 
     public static void main(String[] args) {
         if (sottoStringa(args[0], args[1])) {
@@ -6,29 +7,29 @@ public class Sottostringa {
         } else {
             System.out.println(args[1] + " non è sottostringa di " + args[0]);
         }
-        //System.out.println(sottoStringa(null, null));
-        //System.out.println(sottoStringaTotale(null, null));
+        // System.out.println(sottoStringa(null, null));
+        // System.out.println(sottoStringaTotale(null, null));
     }
 
     // Procedura parziale
-    // REQUIRES: testo, parola not null
-    // MODIFIES: /
-    // EFFECTS: return true if parola is substring of testo,
-    // false otherwise. 
-    // Not case sensity (e.g. sottoStringa(Seat, eaT) returns true)
     public static boolean sottoStringa(String testo, String parola) {
+        // REQUIRES: testo, parola not null
+        // MODIFIES: /
+        // EFFECTS: return true if parola is substring of testo,
+        // false otherwise.
+        // Not case sensity (e.g. sottoStringa(Seat, eaT) returns true)
         testo = testo.toLowerCase();
         parola = parola.toLowerCase();
         return testo.contains(parola);
     }
 
     // Procedura totale
-    // MODIFIES: /
-    // EFFECTS: return true if parola is substring of testo,
-    // false otherwise (even if testo or parola are null)
     public static boolean sottoStringaTotale(String testo, String parola) {
-         if (testo == null || parola == null) 
-           return false;
+        // EFFECTS: return true if parola is substring of testo,
+        // false otherwise (including the case where testo and/or parola are null).
+        // Not case sensity (e.g. sottoStringa(Seat, eaT) returns true)
+        if (testo == null || parola == null)
+            return false;
         testo = testo.toLowerCase();
         parola = parola.toLowerCase();
         return testo.contains(parola);
--- Laboratori/Lab2/5.AstrazioniProcedurali/2.Fattoriale/Fattoriale.java
+++ Laboratori/Lab2/5.AstrazioniProcedurali/2.Fattoriale/Fattoriale.java
@@ -1,10 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fattoriale {
+    //OVERVIEW: static class that contains methods to deal with factorials
     
     public static void main(String[] args) {
-        
+        stampaFattoriali(listaFattoriali(Integer.parseInt(args[0])));
     }
 
     public static int fattoriale(int n) {
+        //EFFECTS: Returns n! if n >= 1. Returns -1 otherwise.
+
+        if (n < 1)
+            return -1;
+
+        int sum = 1;
+        for(int i = 2; i <= n; i++) 
+            sum *= i;
+
+        return sum;
+    }
+
+    public static List<Integer> listaFattoriali(int n) {
+        //EFFECTS: Returns a List containing factorials from 1! to n!
+        //if n is < 1 the returned list is null
+        if (n < 1)
+            return null;
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(fattoriale(i));
+        }
+
+        return list;
+
+    }
+
+    public static void stampaFattoriali(List<Integer> lista) {
+        //EFFECTS: prints a list of factorials in the format n: n!
+        //It prints nothing if list is empty or null
+        if (lista == null)
+            return;
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println((i+1) + ": " + lista.get(i));
+        }
 
     }
 
