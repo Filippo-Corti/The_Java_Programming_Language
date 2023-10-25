3fcd14521e8608aa1f7be9a8d033d5a668f96831
Filippo <corti.filippo03@gmail.com>
2023-10-25 20:26:27 +0200

    Lab4 init

---
+++ Laboratori/Lab4/3.Evento/Evento.java
@@ -0,0 +1,136 @@
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Evento implements Cloneable {
+    // OVERVIEW: modella un Evento con data e nome. Astrazione immutabile
+
+    // attributes
+    Date data;
+    String nome;
+
+    // constructors
+    public Evento(Date data, String nome) throws IllegalArgumentException {
+        // MODIFIES: this
+        // EFFECTS: inizializza this
+        // se data = null lancia NullPointerException
+        // se nome = null lancia NullPointerException
+        // se nome vuoto lancia IllegalArgumentException
+        if (data == null)
+            throw new NullPointerException("Data nulla");
+
+        if (nome == null)
+            throw new NullPointerException("Nome nullo");
+
+        if (nome.equals(""))
+            throw new IllegalArgumentException("Nome vuoto");
+
+        this.data = (Date) data.clone();
+        this.nome = nome;
+
+        assert repOk();
+    }
+
+    // methods
+    public Date getData() {
+        return (Date) data.clone();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean repOk() {
+        if (this.data == null)
+            return false;
+
+        if (this.nome == null)
+            return false;
+
+        if (this.nome.equals(""))
+            return false;
+
+        return true;
+    }
+
+    public Evento copiaEvento(int n) throws CloneNotSupportedException {
+        // EFFECTS: crea un nuovo evento con this.nome e this.data + n
+        // se n <= 0 lancia IllegalArgumentException
+        if (n <= 0)
+            throw new IllegalArgumentException("n <= 0");
+
+        Evento nuovo = (Evento) this.clone();
+        nuovo.data = new Date(nuovo.data.getTime() + TimeUnit.DAYS.toMillis(n));
+        return nuovo;
+
+        // return new Evento(new Date(this.data.getTime() + TimeUnit.DAYS.toMillis(n)),
+        // this.nome);
+    }
+
+    @Override
+    public Object clone() {
+        Evento nuovo = null;
+        try {
+         nuovo = (Evento) super.clone();
+        } catch (CloneNotSupportedException e) {
+            nuovo = new Evento(data, nome);
+        }
+        // Eventuali attributi mutabili devono essere clonati
+        nuovo.data = (Date) data.clone();
+
+        return nuovo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Evento other = (Evento) obj;
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Inserisci data del primo evento");
+        String[] pieces = s.next().split("/");
+
+        Date date1 = new Date(Integer.parseInt(pieces[2]) - 1900,
+                Integer.parseInt(pieces[1]),
+                Integer.parseInt(pieces[0]));
+
+        System.out.println("Inserisci nome del primo evento");
+        String n1 = s.next();
+
+        System.out.println("Inserisci data del secondo evento");
+        pieces = s.next().split("/");
+
+        Date date2 = new Date(Integer.parseInt(pieces[2]) - 1900,
+                Integer.parseInt(pieces[1]),
+                Integer.parseInt(pieces[0]));
+
+        System.out.println("Inserisci nome del secondo evento");
+        String n2 = s.next();
+
+        Evento e1 = new Evento(date1, n1);
+        Evento e2 = new Evento(date2, n2);
+
+        System.out.println(e1.equals(e2));
+
+    }
+
+}
---
+++ Laboratori/Lab4/6.Tris/Tris.java
@@ -0,0 +1,131 @@
+import java.util.Scanner;
+
+public class Tris {
+    // OVERVIEW: classe mutabile che implementa il gioco del Tris tra due giocatori
+    // reali
+
+    // attributes
+    char[][] tabellone;
+    char turno;
+    int contaMosse;
+
+    final char CERCHIO = 'O';
+    final char ICS = 'X';
+    final char VUOTO = 0;
+
+    // constructors
+    public Tris() {
+        // EFFECTS: inizializza Tris ad un tabellone vuoto, con turno del giocatore 'O';
+        tabellone = new char[3][3];
+        turno = CERCHIO;
+
+        assert repOk();
+    }
+
+    public boolean repOk() {
+        if (contaMosse > 9)
+            return false;
+        if (turno != CERCHIO && turno != ICS)
+            return false;
+        if (tabellone == null)
+            return false;
+        return true;
+    }
+
+    public char turno() {
+        // EFFECTS: ritorna il turno corrente ('X' o 'O')
+        return turno;
+    }
+
+    public void cambiaTurno() {
+        // MODIFIES: this.turno
+        // EFFECTS: inverte il turno
+        if (this.turno == ICS)
+            this.turno = CERCHIO;
+        else
+            this.turno = ICS;
+
+        assert repOk();
+    }
+
+    public void mossa(int x, int y) throws IllegalArgumentException {
+        // MODIFIES: this.tabellone, this.turno, this.mosse
+        // EFFECTS: inserisce turno in tabellone[x-1][y-1]. Poi incrementa il contatore
+        // di mosse di 1 e cambio turno
+        // se la casella è occupata lancia IllegalArgumentException
+        // se x e y non sono compresi tra 1 e 3 lancia IllegalArgumentException
+
+        if (x < 1 || x > 3 || y < 1 || x > 3)
+            throw new IllegalArgumentException("Coordinate non valide");
+
+        if (tabellone[x - 1][y - 1] != VUOTO) {
+            throw new IllegalArgumentException("Casella già occupata");
+        }
+
+        tabellone[x - 1][y - 1] = this.turno;
+
+        this.contaMosse++;
+
+        assert repOk();
+    }
+
+    @Override
+    public String toString() {
+        String res = "\n-----------------\n|R\\C| 1 | 2 | 3 |\n-----------------\n";
+        for (int i = 0; i < 3; i++) {
+            res += "| " + (i + 1) + " |";
+            for (int j = 0; j < 3; j++) {
+                res += " " + tabellone[i][j] + " |";
+            }
+            res += "\n-----------------\n";
+        }
+        return res;
+    }
+
+    public boolean terminato() {
+        // EFFECTS: ritorna true se sono già state effettuate 9 mosse;
+        return (this.contaMosse == 9);
+    }
+
+    public boolean vittoria() {
+        // EFFECTS: ritorna true se il giocatore di turno ha vinto. False altrimenti
+        for (int i = 0; i < 3; i++) {
+            // Riga
+            if (quadrEqual(this.turno, tabellone[i][0], tabellone[i][1], tabellone[i][2]))
+                return true;
+            // Colonna
+            if (quadrEqual(this.turno, tabellone[0][i], tabellone[1][i], tabellone[2][i]))
+                return true;
+        }
+        // Prima diagonale
+        if (quadrEqual(this.turno, tabellone[0][0], tabellone[1][1], tabellone[2][2]))
+            return true;
+        // Seconda diagonale
+        if (quadrEqual(this.turno, tabellone[0][2], tabellone[1][1], tabellone[2][0]))
+            return true;
+
+        return false;
+    }
+
+    private boolean quadrEqual(char a, char b, char c, char d) {
+        return (a == b && b == c && c == d);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Tris tris = new Tris();
+        System.out.println(tris);
+        while (!tris.terminato()) {
+            System.out.println("Mossa di " + tris.turno());
+            System.out.println("Inserisci: x y");
+            tris.mossa(s.nextInt(), s.nextInt());
+            System.out.println(tris);
+            if (tris.vittoria()) {
+                System.out.println("Ha vinto " + tris.turno());
+                System.exit(0);
+            }
+            tris.cambiaTurno();
+        }
+        System.out.println("Tabellone riempito");
+    }
+}
